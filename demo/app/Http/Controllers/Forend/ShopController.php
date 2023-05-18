4657c458c68201967429bd94747c1867cd26e86b
nguyên <quocnguyentran.240903@gmail.com>
2023-05-19 00:12:11 +0700

    sua phan trang

--- demo/app/Http/Controllers/Forend/ShopController.php
+++ demo/app/Http/Controllers/Forend/ShopController.php
@@ -14,36 +14,6 @@ class ShopController extends Controller
         return view('fordend.shop.show', compact('products'));
     }
     public function index2(Request $request){
-
-        $sortBy = $request->sort_by ?? 'latest';
-
-        $search = $request->seach ?? '';
-
-        $products = Product::where('name' , 'like' , '%' .$search .'%');
-
-
-        switch($sortBy) {
-            case 'latest':
-                $products = $products->orderBy('id');
-                break;
-            case 'oldest':  
-                $products = $products->orderByDesc('id');
-                break;
-            case 'name-ascending':
-                $products = $products->orderBy('name');
-                break;
-            case 'name-descending':
-                $products = $products->orderByDesc('name');
-                break;
-            case 'price-ascending':
-                $products = $products->orderBy('price');
-                break;
-            case 'price-descending' : 
-                $products = $products->orderByDesc('price');
-            default:
-                $products = $products->orderBy('id') ;
-                           
-        }
         $products = Product::paginate(4);
         return view('fordend.shop.index2' , compact('products'))->with('i' ,(request()->input('page' , 1)-1)*4);
 
